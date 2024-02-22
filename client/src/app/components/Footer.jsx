c016401b25070b6f41febfe2c7cd20ac0b533d8e
Rol24 <dzhansuzyan02@inbox.ru>
2024-02-22 18:11:21 +0300

    rolan

--- client/src/app/components/Footer.jsx
+++ client/src/app/components/Footer.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import titleLogo from '../../assets/svg/titleLogo.svg';
 
 const Footer = () => {
   return (
-    <div>Футер</div>
-  )
-}
+    <div className="footer">
+      <div className="container">
+        <div className="footerWrapper">
+          <div className="footerWrapper__up">
+            <ul className="footerUp__list">
+              <li className="footerUp__item footerUp__itemLeft">
+                <p className="footerTitle">
+                  <span>Follow Us</span>
+                  <img className="footerBublik" src={titleLogo} alt="#" />
+                </p>
+              </li>
+              <li className="footerUp__item footerUp__itemCenter"></li>
+              <li className="footerUp__item footerUp__itemRight"></li>
+            </ul>
+          </div>
+          <div className="footerWrapper__down"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/app/components/Header.jsx
+++ client/src/app/components/Header.jsx
@@ -1,27 +1,75 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import Svg from "../../assets/svg/Svg";
+import React from 'react';
+import { Link, a } from 'react-router-dom';
+import Svg from '../../assets/svg/Svg';
+import logo from '../../assets/images/Bellaria_logo.png';
+import bag from '../../assets/svg/bag.svg';
+import search from '../../assets/svg/search.svg';
 
-import Deliver from "../../assets/svg/delivery.png";
+import Deliver from '../../assets/svg/delivery.png';
 
 const Header = () => {
-  const links = [
-    "Новинки",
-    "Сноуборд",
-    "Лыжи",
-    "Скейт",
-    "Лонгборд",
-    "Вейкборд",
-    "Серф",
-    "Одежда",
-    "Обувь",
-    "Аксессуары",
-    "Бренды",
-    "Распродажа",
-  ];
-
   return (
     <div className="header">
+      <div className="container">
+        <div className="header__inner">
+          <div className="headerMain">
+            <ul className="header__list header__list-left">
+              <li className="header__item header__item-left">
+                <a className="header__linkLeft" to="/catalog">
+                  Главная
+                </a>
+              </li>
+              <li className="header__item header__item-left">
+                <a className="header__linkLeft" to="/catalog">
+                  Страницы
+                </a>
+              </li>
+              <li className="header__item header__item-left">
+                <a className="header__linkLeft" to="/catalog">
+                  Портфолио
+                </a>
+              </li>
+            </ul>
+            <div className="header__logo">
+              <div className="header__logo-wrapper">
+                <img src={logo} alt="Logo" className="header__logotip" />
+              </div>
+            </div>
+            <ul className="header__list header__list-right">
+              <li className="header__item header__item-right">
+                <a className="header__linkRight" to="/catalog">
+                  Блог
+                </a>
+              </li>
+              <li className="header__item header__item-right">
+                <a className="header__linkRight" to="/catalog">
+                  Магазин
+                </a>
+              </li>
+              <li className="header__item header__item-right">
+                <a className="header__linkRight" to="/catalog">
+                  Контакты
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="headerMenu">
+            <div class="headerMenu__barList">
+              <div class="headerMenu__barList-basket">
+                <div class="basket__wrapper">
+                  <p class="basket__wrapper-message">No products in the cart.</p>
+                </div>
+              </div>
+              <a class="headerMenu__barList-icon" title="View your shopping cart">
+                <img className="bag" src={bag} />
+              </a>
+            </div>
+            <div class="headerMenu__searchButton">
+              <img className="search" src={search} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
